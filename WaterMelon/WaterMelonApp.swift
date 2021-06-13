8df3000da64e876212969ef0c36a8b9d16958742
takesi <nanasetakesi@yahoo.co.jp>
2021-06-13 22:02:32 +0800

    Initial Commit

---
+++ WaterMelon/WaterMelonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterMelonApp.swift
+//  WaterMelon
+//
+//  Created by TAKESI NANASE on 2021/6/13.
+//
+
+import SwiftUI
+
+@main
+struct WaterMelonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
